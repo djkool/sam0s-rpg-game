6fbd3d3ce85bb856affa6168b071ce008e4f7039
djkool <andrewpeterson86@gmail.com>
2015-09-26 19:28:00 -0400

    Refactored game world logic into level module.
    This will eventually allow for more complex levels/maps and starts
    to isolate various game logic into their respective modules.
    
    Players can no longer leave the map boundaries.

--- game.py
+++ game.py
@@ -1,28 +1,8 @@
 import pygame
 from pygame.locals import *
 
-pygame.init()
+from level import *
 
-screen=pygame.display.set_mode((660,450))
-
-tiles = pygame.sprite.Group()
-
-world=pygame.image.load('world/r00.png')
-
-clock=pygame.time.Clock()
-
-def getarea(posx,posy):
-    x=posx*60
-    y=posy*30
-
-    sprite = world.subsurface(pygame.Rect(x, y, 60, 30)) #grabs the sprite at this location
-
-    return sprite
-
-class Entity(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        
 class Controller(object):
     def __init__(self):
         self.area=[0,0]
@@ -31,12 +11,12 @@ class Controller(object):
             self.area[0]+=1
             print "lel"
         tiles.update()
-class Player(object):
+
+
+class Player(Entity):
     def __init__(self,x,y):
-        self.x=x
-        self.y=y
+        Entity.__init__(self, x, y)
     def update(self):
-        pygame.draw.circle(screen, (0,235,50), (self.x,self.y), 16, 0)
         k=pygame.key.get_pressed()
         if k[K_w]:
             self.y-=5
@@ -46,76 +26,59 @@ class Player(object):
             self.y+=5
         if k[K_d]:
             self.x+=5
-            
-class Tile (Entity): 
-    def __init__ (self,x,y,color):
-        Entity.__init__(self)
-        self.color=color
-        self.x=x
-        self.y=y
-    def update(self):
-        pygame.draw.rect(screen, (self.color), (self.x,self.y,16,16), 0)
-        
-levelsurf=pygame.Surface((660,450))
 
-def CreateLevel(LEVELX,LEVELY):
-    levelsurf.fill((0,0,255))
-    limage=getarea(LEVELX,LEVELY)
-    for j in range(30):
-        for i in range(60):
-            #print str(i)+","+str(j)
-            px = limage.get_at((i,j))
-            if px[0]==185:
-                #t = Tile(i*16,j*16,(185,122,87))
-                pygame.draw.rect(levelsurf, (128,122,87), (i*11,j*15,11,15), 0)
-                #tiles.add(t)
-            if px[0]==32:
-                pygame.draw.rect(levelsurf, (255,255,87), (i*11,j*15,11,15), 0)
-                #t = Tile(i*16,j*16,(255,255,0))
-                #tiles.add(t)
-            if px[2]==176:
-                pygame.draw.rect(levelsurf, (239,228,176), (i*11,j*15,11,15), 0)
-                #t = Tile(i*16,j*16,(239,228,176))
-                #tiles.add(t)
-        
+    def render(self, surf):
+        screen_pos = (self.x % SCREEN_SIZE[0], self.y % SCREEN_SIZE[1])
+        pygame.draw.circle(surf, (0,235,50), screen_pos, 16, 0)
+
+
+SCREEN_SIZE = (660,450)
+DESIRED_FPS = 90
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+tiles = pygame.sprite.Group()
+world = Level(SCREEN_SIZE, "r00.png")
+clock = pygame.time.Clock()
 
 go=True
-WORLDX=0
-WORLDY=0
-CreateLevel(0,0)
 p=Player(64,64)
 c=Controller()
 
 
 while go:
     pygame.display.set_caption(str(clock.get_fps()))
-    screen.fill((0,0,0))
+
+    # updates
     #c.update(p)
     #screen.blit(limages,(0,0))
-    screen.blit(levelsurf,(0,0))
     p.update()
+
+    # Keep player from leaving game world
+    # This should eventually be moved into Player
+    if p.x < 0:
+        p.x = 1
+    elif p.x >= world.size[0]:
+        p.x = world.size[0] - 1
+    if p.y < 0:
+        p.y = 1
+    elif p.y >= world.size[1]:
+        p.y = world.size[1] - 1
+
+    world.move((p.x, p.y))
    
     for e in pygame.event.get():
         if e.type == QUIT:
             go = False
-    clock.tick(90)
+
+    # render
+    screen.fill((0,0,0))
+    world.render(screen)
+    p.render(screen)
     pygame.display.flip()
-    if p.x>640:
-        p.x=1
-        WORLDX+=1
-        CreateLevel(WORLDX,WORLDY)
-    if p.x<0:
-        p.x=639
-        WORLDX-=1
-        CreateLevel(WORLDX,WORLDY)
-    if p.y<0:
-        p.y=479
-        WORLDY-=1
-        CreateLevel(WORLDX,WORLDY)
-    if p.y>480:
-        p.y=1
-        WORLDY+=1
-        CreateLevel(WORLDX,WORLDY)
+
+    clock.tick(DESIRED_FPS)
+
 #print tiles.sprites()
 
 pygame.display.quit()
